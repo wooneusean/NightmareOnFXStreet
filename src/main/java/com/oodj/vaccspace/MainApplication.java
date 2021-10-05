e9b7bef1beb6e42808c6062c8de150cf5022c57f
Woon Eusean <eusean.woon@infinitilab.com>
2021-10-05 17:33:30 +0800

    🧹: Fix some font issues

--- src/main/java/com/oodj/vaccspace/MainApplication.java
+++ src/main/java/com/oodj/vaccspace/MainApplication.java
@@ -3,6 +3,7 @@ package com.oodj.vaccspace;
 import com.oodj.vaccspace.utils.Navigator;
 import javafx.application.Application;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import textorm.TextORM;
 
@@ -19,7 +20,15 @@ public class MainApplication extends Application {
     public void start(Stage stage) {
         TextORM.setStoragePath("storage");
         TextORM.setMetaStoragePath("storage");
+
+        // Set icons
         stage.getIcons().add(new Image(Objects.requireNonNull(MainApplication.class.getResourceAsStream("images/vaccine.png"))));
+
+        // Load fonts
+        Font.loadFont(getClass().getResourceAsStream("fonts/Poppins-Medium.ttf"), 16);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Poppins-Regular.ttf"), 16);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Roboto-Medium.ttf"), 16);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Roboto-Regular.ttf"), 16);
 
         try {
             Navigator.init(stage, "login");
