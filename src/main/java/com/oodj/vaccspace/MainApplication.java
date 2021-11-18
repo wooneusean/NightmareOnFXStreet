3f200f9f9bb6a6bb7a24cc9f9f2f10b948988acb
Woon Eusean <eusean.woon@infinitilab.com>
2021-11-18 19:18:51 +0800

    🐤: Updated "Add Appointment Page"

--- src/main/java/com/oodj/vaccspace/MainApplication.java
+++ src/main/java/com/oodj/vaccspace/MainApplication.java
@@ -23,6 +23,8 @@ public class MainApplication extends Application {
         TextORM.setStoragePath("storage");
         TextORM.setMetaStoragePath("storage");
 
+        // seedModels();
+
         // Set icons
         stage.getIcons().add(new Image(Objects.requireNonNull(MainApplication.class.getResourceAsStream("images/vaccine_small.png"))));
 
@@ -36,6 +38,7 @@ public class MainApplication extends Application {
         stage.setResizable(false);
 
         try {
+            // Change initialRoute to test different pages.
             Navigator.init(stage, "login");
         } catch (Exception e) {
             e.printStackTrace();
@@ -43,22 +46,63 @@ public class MainApplication extends Application {
     }
 
     private void seedModels() {
-        Citizen citizen = new Citizen("qwe", "0123456789", "qwe@mail.com", "qwe", VaccinationStatus.NOT_REGISTERED, "010203040506");
-        citizen.save();
+        // Citizen
+        Citizen citizen1 = new Citizen("qwe", "0123456789", "qwe@mail.com", "qwe", VaccinationStatus.NOT_REGISTERED, "010203040506");
+        citizen1.save();
 
-        VaccinationCenter vc = new VaccinationCenter("Bukit Jalil", CenterStatus.OPEN);
-        vc.save();
+        Citizen citizen2 = new Citizen("asd", "0123456789", "asd@mail.com", "asd", VaccinationStatus.AWAITING_FIRST_DOSE, "010203040506");
+        citizen2.save();
 
-        VaccineType vt = new VaccineType("Sinovac", 2);
-        vt.save();
+        // Center
 
-        VaccineBatch vb = new VaccineBatch(vt.getId(), 200, vc.getId(), LocalDate.now());
-        vb.save();
+        VaccinationCenter bukitJalil = new VaccinationCenter("Bukit Jalil", CenterStatus.OPEN);
+        bukitJalil.save();
 
-        Vaccine vaccine = new Vaccine(vb.getId(), LocalDate.now(), VaccineStatus.AVAILABLE);
-        vaccine.save();
+        VaccinationCenter movenpick = new VaccinationCenter("Movenpick", CenterStatus.OPEN);
+        movenpick.save();
 
-        new Appointment(citizen.getId(), vc.getId(), vaccine.getId(), LocalDate.now(), AppointmentStatus.CONFIRMED, Dose.FIRST).save();
+        // Types
+
+        VaccineType sinovac = new VaccineType("Sinovac", 2);
+        sinovac.save();
+
+        VaccineType pfizer = new VaccineType("Pfizer", 2);
+        pfizer.save();
+
+        VaccineType johnson = new VaccineType("Johnson & Johnson", 2);
+        johnson.save();
+
+        // Batches
+
+        VaccineBatch sinovacFirstBatch = new VaccineBatch(sinovac.getId(), 200, bukitJalil.getId(), LocalDate.now());
+        sinovacFirstBatch.save();
+
+        VaccineBatch sinovacSecondBatch = new VaccineBatch(sinovac.getId(), 200, bukitJalil.getId(), LocalDate.now());
+        sinovacSecondBatch.save();
+
+        VaccineBatch pfizerBatch1 = new VaccineBatch(pfizer.getId(), 250, bukitJalil.getId(), LocalDate.now());
+        pfizerBatch1.save();
+
+        VaccineBatch pfizerBatch2 = new VaccineBatch(pfizer.getId(), 250, movenpick.getId(), LocalDate.now());
+        pfizerBatch2.save();
+
+        VaccineBatch johnsonBatch1 = new VaccineBatch(johnson.getId(), 125, movenpick.getId(), LocalDate.now());
+        johnsonBatch1.save();
+
+        // Vaccines
+
+        Vaccine sinovacVaccine = new Vaccine(sinovacFirstBatch.getId(), LocalDate.now(), VaccineStatus.AVAILABLE);
+        sinovacVaccine.save();
+
+        Vaccine pfizerVaccine = new Vaccine(pfizerBatch1.getId(), LocalDate.now(), VaccineStatus.AVAILABLE);
+        pfizerVaccine.save();
+
+        Vaccine johnsonVaccine = new Vaccine(johnsonBatch1.getId(), LocalDate.now(), VaccineStatus.USED);
+        johnsonVaccine.save();
+
+        new Appointment(citizen1.getId(), bukitJalil.getId(), sinovacVaccine.getId(), LocalDate.now(), AppointmentStatus.CONFIRMED, Dose.FIRST).save();
+
+        new Appointment(citizen2.getId(), movenpick.getId(), johnsonVaccine.getId(), LocalDate.now(), AppointmentStatus.AWAITING_CONFIRMATION, Dose.FIRST).save();
     }
 }
 
