57bb536433dc329a436331e3fae03dac2686ac5e
euseanwoon2016 <20278298+euseanwoon2016@users.noreply.github.com>
2021-11-22 17:58:57 +0800

    🐴: Added cancellation of appointments

--- src/main/java/com/oodj/vaccspace/Global.java
+++ src/main/java/com/oodj/vaccspace/Global.java
@@ -1,7 +1,21 @@
 package com.oodj.vaccspace;
 
+import com.oodj.vaccspace.controllers.dashboard.DashboardController;
+import com.oodj.vaccspace.models.Citizen;
+import textorm.TextORM;
+
 public class Global {
+    private static DashboardController dashboardReference;
+
     private static int userId;
+
+    public static DashboardController getDashboardReference() {
+        return dashboardReference;
+    }
+
+    public static void setDashboardReference(DashboardController dashboardReference) {
+        Global.dashboardReference = dashboardReference;
+    }
 
     public static int getUserId() {
         return userId;
@@ -9,5 +23,9 @@ public class Global {
 
     public static void setUserId(int userId) {
         Global.userId = userId;
+    }
+
+    public static Citizen getLoggedInUser() {
+        return TextORM.getOne(Citizen.class, data -> Integer.parseInt(data.get("id")) == userId);
     }
 }
