f4ccc0a10c0bdb91560d2688dc30c17db21aef50
euseanwoon2016 <20278298+euseanwoon2016@users.noreply.github.com>
2021-11-25 16:13:53 +0800

    ✨: Added page to register people for committee

--- src/main/java/com/oodj/vaccspace/models/User.java
+++ src/main/java/com/oodj/vaccspace/models/User.java
@@ -22,11 +22,25 @@ public abstract class User extends Model {
     public User() {
     }
 
+    /**
+     * Creates a new User object
+     *
+     * @param name     name for the user
+     * @param phone    phone number of the user
+     * @param email    email for the user
+     * @param password password to login, if {@code null} value provided, defaults to {@code email#xxxx} where *
+     *                 {@code xxxx} is the last 4 digits of phone number
+     */
     public User(String name, String phone, String email, String password) {
         this.name = name;
         this.phone = phone;
         this.email = email;
-        this.password = password;
+
+        if (password == null) {
+            this.password = String.format("%s#%s", this.email, this.phone.substring(this.phone.length() - 4));
+        } else {
+            this.password = password;
+        }
     }
 
     public String getName() {
